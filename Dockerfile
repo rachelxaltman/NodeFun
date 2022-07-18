872a576c51b10a1d003418ae2162933bfc3fbd2b
Rachel Altman <rachel8222@gmail.com>
2022-07-18 13:07:41 -0400

    Add

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:16
+
+WORKDIR /app
+
+COPY package*.json ./
+
+ADD  ./app /app
+
+RUN npm install morgan mongoose express connect-flash method-override express-validator express-rate-limit bcrypt
+
+COPY . .
+
+EXPOSE 8080
+
+CMD [ "node", "app.js" ]
+
+
---
+++ app/app.js
@@ -0,0 +1,127 @@
+//require modules
+const express = require('express');
+const connectionRoutes = require('./routes/connectionRoutes')
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const userRoutes = require('./routes/userRoutes');
+
+//create app
+const app = express();
+
+//configure app
+let port = 8080;
+let host = 'localhost';
+app.set('view engine', 'ejs');
+
+//connect to database
+mongoose.connect('mongodb://localhost:27017/demos', 
+                {useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    //start app
+    app.listen(port, host, ()=>{
+        console.log('Server is running on port', port);
+    });
+})
+.catch(err=>console.log(err.message));
+
+// //mount middlware
+app.use(
+    session({
+        secret: "ajfeirf90aeu9eroejfoefj",
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({mongoUrl: 'mongodb://localhost:27017/demos'}),
+        cookie: {maxAge: 60*60*1000}
+        })
+);
+app.use(flash());
+
+ app.use((req, res, next) => {
+     //console.log(req.session);
+     res.locals.user = req.session.user||null;
+     res.locals.firstName = req.session.firstName||'Guest';
+     res.locals.errorMessages = req.flash('error');
+     res.locals.successMessages = req.flash('success');
+     next();
+ });
+
+
+//mount middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+
+
+// set up routes
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+// set up routes
+app.get('/about', (req, res)=>{
+    res.render('connection/about');
+});
+
+
+// set up new
+app.get('/new', (req, res)=>{
+    res.render('connection/new');
+});
+
+// set up routes
+app.get('/contact', (req, res)=>{
+    res.render('connection/contact');
+});
+
+// set up routes
+app.get('/connectionDetail', (req, res)=>{
+    res.render('connection/connectionDetail');
+});
+
+
+app.use('/connections', connectionRoutes);
+app.use('/users', userRoutes);
+
+
+
+// UN COMMENT LATER
+app.use((req, res, next)=>{
+    let err = new Error('The server cannot locate' +req.url);
+        err.status = 404;
+        next(err);
+
+});
+
+
+
+app.use((err, req, res, next)=>{
+    console.log(err.stack);
+    if (!err.status){
+        err.status = 500;
+        err.message = "Internal Server Error";
+    }
+ res.status(err.status);
+ res.render('error', {error: err});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
